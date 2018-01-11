5e238d5d0fcdb734898104cb49e6e641b5cfcbb7
Nicholas Biddell <nicbiddell@gmail.com>
2018-01-10 19:49:25 -0500

    Add LeaguesHome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { LeaguesHomeComponent } from './leagues-home/leagues-home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomeComponent
+    HomeComponent,
+    LeaguesHomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/leagues-home/leagues-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leagues-home',
+  templateUrl: './leagues-home.component.html',
+  styleUrls: ['./leagues-home.component.css']
+})
+export class LeaguesHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
