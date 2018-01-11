e9717c7a58623665ce19337265715d460042315e
Nicholas Biddell <nicbiddell@gmail.com>
2018-01-10 20:00:33 -0500

    Add new Leagues Component. Will be container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { LeaguesComponent } from './leagues/leagues.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     HomeComponent,
+    LeaguesComponent,
   ],
   imports: [
     BrowserModule
