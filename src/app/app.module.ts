a108782b5c824e2369cf08785b06213261d0eda1
Nicholas Biddell <nicbiddell@gmail.com>
2018-01-10 19:59:36 -0500

    Forget all this

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
-import { LeaguesHomeComponent } from './leagues-home/leagues-home.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     HomeComponent,
-    LeaguesHomeComponent
   ],
   imports: [
     BrowserModule
