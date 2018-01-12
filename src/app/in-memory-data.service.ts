e9405395088c979a0a486218dc93625ee2051ba3
Nicholas Biddell <nicbiddell@gmail.com>
2018-01-11 22:24:33 -0500

    Add service data and env variable

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,9 +3,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         const leagues = [
-            { name: 'Global Starcraft II League', abbreviation: 'GSL' },
-            { name: 'Collegiate Starcraft II League', abbreviation: 'CSL' },
-            { name: 'Chobo Team League', abbreviation: 'CTL' }
+            { id: 1, name: 'Global Starcraft II League', abbreviation: 'GSL' },
+            { id: 2, name: 'Collegiate Starcraft II League', abbreviation: 'CSL' },
+            { id: 3, name: 'Chobo Team League', abbreviation: 'CTL' }
         ];
 
         return {leagues}
--- src/app/league.service.ts
+++ src/app/league.service.ts
@@ -4,11 +4,12 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
+import { environment } from '../../src/environments/environment';
 import { League } from './league.model';
 
 @Injectable()
 export class LeagueService {
-  private leaguesUrl = 'api/leagues';
+  private leaguesUrl = `${environment.baseApiEndpoint}/leagues`;
 
   constructor(private http: HttpClient) { }
 
