2a55a5053907f94298482dd9ff404121362e4ab6
Nicholas Biddell <nicbiddell@gmail.com>
2018-01-11 23:30:17 -0500

    Repair test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,28 @@
+import { Component } from '@angular/core'
 import { TestBed, async } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LeaguesComponent } from './leagues/leagues.component'
+import { AppRoutingModule } from './/app-routing.module';
+import { APP_BASE_HREF } from '@angular/common'
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
+        MockNavigationComponent,
+        LeaguesComponent,
+        HomeComponent,
         AppComponent
       ],
+      imports: [
+        
+        AppRoutingModule
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/'}
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -18,10 +35,22 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
 });
+
+@Component({
+  selector: 'app-navigation',
+  template: ''
+})
+class MockNavigationComponent { }
+
+@Component({
+  selector: 'app-leagues',
+  template: ''
+})
+class MockLeaguesComponent { }
+
+@Component({
+  selector: 'app-home',
+  template: ''
+})
+class MockHomeComponent { }
